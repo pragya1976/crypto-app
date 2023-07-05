2173ec641cab5b53095abf2a7b275942877ff66c
pragya1976 <palpragya688@gmail.com>
2023-07-05 15:42:17 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/header";
+import Home from "./components/home";
+import Coins from "./components/coins";
+import Exchanges from "./components/exchanges";
+import CoinDetails from "./components/coindetails";
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coins" element={<Coins />} />
+        <Route path="/exchanges" element={<Exchanges />} />
+        <Route path="/coin/:id" element={<CoinDetails />} />
+      </Routes>
+
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
